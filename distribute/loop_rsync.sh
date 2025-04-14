b1fb4def3db6299760599d737b02f331fa41249f
liuxynb <2096321485@qq.com>
2025-04-14 10:58:36 +0000

    add .

--- distribute/loop_rsync.sh
+++ distribute/loop_rsync.sh
@@ -4,14 +4,13 @@ FILE="./test"  # 当前目录下生成文件
 
 while true; do
   echo "[*] Generating 256MB file in current directory..."
-  dd if=/dev/zero of="$FILE" bs=1M count=256 status=none
+  dd if=/dev/zero of="$FILE" bs=1M count=512 status=none
 
-  echo "[*] Removing old file on node01..."
-  ssh femu@node01 'rm -f /mnt/nvme0/D_1_0'
+  echo "[*] Removing old file on node02..."
+  ssh femu@node02 'rm -f /mnt/nvme0/D_1_0'
 
-  echo "[*] Transferring file to node01..."
-  rsync -azP "$FILE" femu@node01:/mnt/nvme0/D_1_0
+  echo "[*] Transferring file to node02..."
+  rsync -azP "$FILE" femu@node02:/mnt/nvme0/D_1_0
 
-  echo "[*] One round done. Sleep 1s."
-  sleep 1
+  echo "[*] One round done."
 done
