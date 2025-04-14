dcf86630d447692048215ff5fc5c0f8618c546c2
liuxynb <2096321485@qq.com>
2025-04-14 11:03:52 +0000

    .

--- distribute/loop_rsync.sh
+++ distribute/loop_rsync.sh
@@ -4,7 +4,7 @@ FILE="./test"  # 当前目录下生成文件
 
 while true; do
   echo "[*] Generating 256MB file in current directory..."
-  dd if=/dev/zero of="$FILE" bs=1M count=512 status=none
+  dd if=/dev/zero of="$FILE" bs=1M count=64 status=none
 
   echo "[*] Removing old file on node02..."
   ssh femu@node02 'rm -f /mnt/nvme0/D_1_0'
