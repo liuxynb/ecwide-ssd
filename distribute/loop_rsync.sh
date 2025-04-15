0f09694d49842193d4228a200eb6a6b2b49eac10
liuxynb <2096321485@qq.com>
2025-04-15 07:30:42 +0000

    add .

--- distribute/loop_rsync.sh
+++ distribute/loop_rsync.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-FILE="./test"  # 当前目录下生成的文件
+FILE="./test"  # 当前目录下生成文件
 COUNT=1        # 初始化计数器
 
-# 第一次执行前先检查文件是否存在，不存在就生成
-if [ ! -f "$FILE" ]; then
-  echo "[*] Generating 64MB file in current directory..."
-  dd if=/dev/zero of="$FILE" bs=1M count=64 status=none
-fi
-
-# 无限循环复用该文件传输
 while true; do
+  echo "[*] Round $COUNT: Generating 64MB file in current directory..."
+  dd if=/dev/zero of="$FILE" bs=1M count=64 status=none
+
+  # echo "[*] Round $COUNT: Removing old file on node01..."
+  # ssh femu@node01 'rm -f /mnt/nvme0/D_1_0'
+
   echo "[*] Round $COUNT: Transferring file to node01..."
   scp "$FILE" femu@node01:/mnt/nvme0/D_1_0
 
