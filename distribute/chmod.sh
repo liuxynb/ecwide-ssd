0e4ee59c478d416641259adaee56e6677e4c42bd
liuxynb <2096321485@qq.com>
2025-04-15 07:04:48 +0000

    fix bug

--- distribute/chmod.sh
+++ distribute/chmod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# 用户、密码与节点数量配置
 USER="femu"
 PASSWORD="femu"
 NODE_PREFIX="node"
 NODE_COUNT=9
 
-# 执行命令
 REMOTE_CMD="sudo chown -R femu:femu /mnt/nvme0 /mnt/nvme1 /mnt/nvme2 /mnt/nvme3 /mnt/nvme4 /mnt/nvme5"
 
-# 执行循环
-for i in $(seq -w 1 $NODE_COUNT); do
-  HOST="${USER}@${NODE_PREFIX}${i}"  # 注意这里用的是 ${i}
+for i in $(seq 1 $NODE_COUNT); do
+  INDEX=$(printf "%02d" $i)
+  NODE_NAME="${NODE_PREFIX}${INDEX}"
+  HOST="${USER}@${NODE_NAME}"
+
   echo "[*] Logging into $HOST and running command..."
 
   sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$HOST" "$REMOTE_CMD"
