359ec33449a9b645a721eed6da08a7638538e92a
liuxynb <2096321485@qq.com>
2025-04-15 09:10:31 +0000

    .

--- distribute/update_and_send_G_1_0.sh
+++ distribute/update_and_send_G_1_0.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-LOCAL_FILE="/home/femu/ecwide-ssd/test/chunks/G_1_0"
+LOCAL_FILE="/home/femu/ecwide-ssd/test/chunks/D_1_0"
 REMOTE_USER="femu"
 REMOTE_HOST="node01"
-REMOTE_PATH="/mnt/nvme3/G_1_0"
+REMOTE_PATH="/mnt/nvme0/D_1_0"
 
 while true; do
   # 当前时间戳
   TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-  echo "[*] Updating file G_1_0 at $TIMESTAMP..."
+  echo "[*] Updating file D_1_0 at $TIMESTAMP..."
 
   # 获取本地文件大小（若文件不存在，则默认 1MB）
   FILESIZE=$(stat -c%s "$LOCAL_FILE" 2>/dev/null || echo 1048576)
