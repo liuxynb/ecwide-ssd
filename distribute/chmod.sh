e13cd4efbcb374dba557d2b06701e182b28b681d
liuxynb <2096321485@qq.com>
2025-04-15 07:02:40 +0000

    fix name

--- distribute/chmod.sh
+++ distribute/chmod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# 设置虚拟机用户名、密码和节点数量
+# 用户、密码与节点数量配置
 USER="femu"
 PASSWORD="femu"
 NODE_PREFIX="node"
-NODE_COUNT=9  # 根据你有多少台 nodeXX 设置
+NODE_COUNT=9
 
-# 要执行的命令
+# 执行命令
 REMOTE_CMD="sudo chown -R femu:femu /mnt/nvme0 /mnt/nvme1 /mnt/nvme2 /mnt/nvme3 /mnt/nvme4 /mnt/nvme5"
 
-# 遍历每个节点
+# 执行循环
 for i in $(seq -w 1 $NODE_COUNT); do
-  HOST="$USER@${NODE_PREFIX}${i}"
+  HOST="${USER}@${NODE_PREFIX}${i}"  # 注意这里用的是 ${i}
   echo "[*] Logging into $HOST and running command..."
 
   sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$HOST" "$REMOTE_CMD"
